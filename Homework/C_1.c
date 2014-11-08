6c8a0f03c6ad6d535b1faeb295018b57567366be
Vendin <v.a.5@list.ru>
2014-11-08 22:05:04 +0300

    mod1

--- Homework/C_1.c
+++ Homework/C_1.c
@@ -4,35 +4,34 @@
 #include <math.h>
 #include <ctype.h>
 
-void vive(int *stroka, int razmer) { /* Функция вывода*/
+void vive(int *stroka, int razmer) {
     int w;
     for (w = razmer - 1; w != -1; w--) {
         if (stroka[w] > 9) {
-            printf("%c", (char) (stroka[w] + 55));
+            printf("%c", (char)(stroka[w] + 55));
         } else {
             printf("%i", stroka[w]);
         }
     }
 }
 
-void ten_in_q(int basis, int number) { /* Перевод из 10 систему счисления по основания q*/
-    int *si=malloc(sizeof(int));      /*Функция принимает десятичное число   */
-    if(si!=NULL){                    /* и систему в которую нужно перевести */
-    int de = number, i;             /* это число */  
+void ten_in_q(int basis, int number) {
+    int *si = malloc(sizeof(int));
+    if (si != NULL) {
+    int de = number, i;
     for (i = 0; de != 0; i++) {
-        si=realloc(si,(i+1)*sizeof(int));           
+        si = realloc(si, (i+1)*sizeof(int));           
         *(si + i) = de % basis;
         de /= basis;
     }
-    
-    vive(si,i); /*выводит значение */
+    vive(si,i); 
     free(si);
   }
 }
 
-int q_in_ten(char *number,int basis) {/* Перевод из системы счисления по основания q* в 10*/
-    int se = 0, i;                   /*Принимает значение числа в системе и значение*/
-    for (i = 0; i < strlen(number); i++) {{/*системы счисления и переводит в 10 */
+int q_in_ten(char *number,int basis) {
+    int se = 0, i;                  
+    for (i = 0; i < strlen(number); i++) {{
         if (number[i] >= '0' && number[i] <= '9') 
             se += (((int)number[i]) - 48) * pow(basis, (strlen(number) - 1 - i));
         }
@@ -43,28 +42,28 @@ int q_in_ten(char *number,int basis) {/* Перевод из системы сч
             se += (((int)number[i])-87) * pow(basis, (strlen(number) - 1 - i));
         }
     }
-    return se; // возвращает 10 значение числа
+    return se; 
 }
 
-int check(int basis_start, int basis_end, char *number) { /*Функция всех проверок */
+int check(int basis_start, int basis_end, char *number) { 
     if (strlen(number) == 1 && number[0] == '0') {
         printf("0");
         free(number);
         return 1;
     }
-    if ((basis_start >= 2 && basis_start <= 36) == 0) { /*Проверка на облость вхождения */
+    if ((basis_start >= 2 && basis_start <= 36) == 0) {
         free(number);
         printf("[error]");
         return 1;
     }
-    if ((basis_end >= 2 && basis_end <= 36) == 0) { /*Проверка на облость вхождения */
+    if ((basis_end >= 2 && basis_end <= 36) == 0) {
         printf("[error]");
         free(number);
         return 1;
     }
     int j;
 
-    for (j = 0; j < strlen(number); j++) {/* Проверка вхождения символов в начальную СЧ*/
+   for (j = 0; j < strlen(number); j++) {
         if ((number[j] >= '0' && (number[j] <= '0' + (basis_start >= 10 ? 9 : basis_start-1)|| (basis_start > 10 ? (number[j] >= 'A' && number[j] < 'A' + basis_start - 10) : 0) || (basis_start > 10 ? (number[j] >= 'a' && number[j] < 'a' + basis_start - 10) : 0))) == 0 ) {
             printf("[error]");
             free(number);
@@ -75,22 +74,22 @@ int check(int basis_start, int basis_end, char *number) { /*Функция вс�
 }
 
 int main() {
-    int P, Q,i=0;
-    char *S = malloc(sizeof(char));
-    if(S!=NULL){
-      if( scanf("%i %i", &P, &Q)==2 ){
+    int P, Q, i = 0;
+    char *S = (char*)malloc(sizeof(char));
+    if(S != NULL){
+      if( scanf("%i %i", &P, &Q) == 2 ){
         char lol;
-        do { 
-         lol=getchar();
-         if (isdigit(lol)){
+        do {
+         lol = getchar();
+         if (isdigit(lol)) {
            i++;
-           S=realloc(S,i);
-           *(S+i-1)=lol;
+           S = realloc(S, i);
+           *(S+i-1) = lol;
           }
-        }while(lol!= '\n'); 
+        } while(lol != '\n'); 
          
         if ( check(P, Q, S) == 0 ) {
-              ten_in_q(Q, q_in_ten(S,P));
+              ten_in_q(Q, q_in_ten(S, P));
               free(S);
         }
     }
